2fbdd223908b8b0016eb1c0c935bdeb80d7a3d72
bharatvayitla <vayitla@uwindsor.ca>
2023-03-04 13:02:24 -0500

    booking model

--- harsh/booking_jumpstart/jumpstart/models.py
+++ harsh/booking_jumpstart/jumpstart/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create your models here.
 
@@ -12,3 +12,34 @@ class Customer(User):
 
     def __str__(self):
         return self.username
+
+
+class Booking(models.Model):
+    TICKET_CHOICES = [
+        ('Adult', '1000'),
+        ('Child', '600'),
+        ('FastTrackAdult', '2000'),
+        ('FastTrackChild', '1200'),
+        ('SeniorCitizen', '800'),
+        ('AdultCollegeIdOffer', '900'),
+    ]
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    bookingDate = models.DateField()
+    reserveDate = models.DateField()
+    address = models.CharField(max_length=100)
+    phoneNumber = models.CharField(max_length=20)
+    typeOfTicketAndPrice = models.CharField(max_length=20, choices=TICKET_CHOICES)
+    totalPrice = models.PositiveIntegerField(default=0)
+    adultTicketCount = models.PositiveIntegerField(default=0)
+    ChildTicketCount = models.PositiveIntegerField(default=0)
+    FastTrackAdultTicketCount = models.PositiveIntegerField(default=0)
+    FastTrackChildTicketCount = models.PositiveIntegerField(default=0)
+    SeniorCitizenTicketCount = models.PositiveIntegerField(default=0)
+    AdultCollegeIdOfferTicketCount = models.PositiveIntegerField(default=0)
+
+
+
+
+
+
+
