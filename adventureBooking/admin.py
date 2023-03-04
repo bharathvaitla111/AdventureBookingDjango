6a44a9d98833dcf3eb2b30b7fea4ad45412c86a8
harsh-525 <108181489+harsh-525@users.noreply.github.com>
2023-03-04 11:43:03 -0500

    del dir

--- adventureBooking/admin.py
+++
@@ -1,9 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-from django.db import models
-from .models import Adventure, Booking, User
-
-# Register your models here.
-admin.site.register(Adventure)
-admin.site.register(Booking)
-admin.site.register(User)
